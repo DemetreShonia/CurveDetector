2e8aafb9c1311fc2809b78311bcc41f9645ed7eb
DemetreShonia <shonka.demeloper@gmail.com>
2023-11-30 00:02:29 +0400

    Refactor a bit

---
+++ Scripts/interpolations.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class InterpolationStrategy:
+    def interpolate(self, x, y):
+        pass
+
+class LinearPieceWiseInterpolation(InterpolationStrategy):
+    def interpolate(self, x, y):
+        for i in range(len(x) - 1):
+            x_start, x_end = x[i], x[i + 1]
+            y_start, y_end = y[i], y[i + 1]
+
+            slope = (y_end - y_start) / (x_end - x_start)
+            intercept = y_start - slope * x_start
+
+            x_interpolated = np.linspace(x_start, x_end, 100)
+            y_interpolated = slope * x_interpolated + intercept
+
+            plt.plot(x_interpolated, y_interpolated, color='red')
+    
+        plt.scatter(x, y)
+
+class LeastSquaresInterpolation(InterpolationStrategy):
+    def interpolate(self, x, y):
+        degree = 3
+        A = np.vander(x, degree + 1, increasing=True)
+
+        # Check matrix rank
+        if np.linalg.matrix_rank(A) == A.shape[1]:  # Check if matrix is full rank
+            # Solve the normal equations to obtain coefficients
+            ATA = np.dot(A.T, A)
+            ATy = np.dot(A.T, y)
+            coeffs = np.linalg.solve(ATA, ATy)
+
+            # Generate points for smooth curve display
+            x_smooth = np.linspace(np.min(x), np.max(x), 100)
+            A_smooth = np.vander(x_smooth, degree + 1, increasing=True)
+            y_smooth = np.dot(A_smooth, coeffs)
+
+            # Plot each segment's points and the fitted polynomial curve
+            plt.scatter(x, y)
+            plt.plot(x_smooth, y_smooth, color='red')
+        else:
+            print("Matrix is not full rank, skipping segment fitting.")
+
+class CubicSplineInterpolation(InterpolationStrategy):
+    def interpolate(self, x, y):
+        def cubic_spline_interpolation(x, y):
+            n = len(x)
+            if n != len(y):
+                print("Interpolation value is outside the range of provided data")
+
+
+            h = np.diff(x)
+            delta = np.diff(y) / h
+
+            # Construct the tridiagonal matrix
+            matrix = np.zeros((n, n))
+            matrix[0, 0] = 1
+            matrix[n - 1, n - 1] = 1
+
+            for i in range(1, n - 1):
+                matrix[i, i - 1] = h[i - 1]
+                matrix[i, i] = 2 * (h[i - 1] + h[i])
+                matrix[i, i + 1] = h[i]
+
+            rhs = np.zeros(n)
+            rhs[1:-1] = 3 * (delta[1:] - delta[:-1])
+
+            # Solve the linear system to find the coefficients
+            c = np.linalg.solve(matrix, rhs)
+
+            # Calculate the remaining coefficients
+            b = np.zeros(n - 1)
+            d = np.zeros(n - 1)
+            for i in range(n - 1):
+                b[i] = delta[i] - h[i] * (2 * c[i] + c[i + 1]) / 3
+                d[i] = (c[i + 1] - c[i]) / (3 * h[i])
+
+            coefficients = []
+            for i in range(n - 1):
+                coefficients.append((y[i], b[i], c[i], d[i]))
+
+            return coefficients
+        
+        def cubic_spline_interpolate(x, coefficients, x_interpolate):
+            for i in range(len(coefficients)):
+                if x_interpolate >= x[i] and x_interpolate <= x[i + 1]:
+                    a, b, c, d = coefficients[i]
+                    t = (x_interpolate - x[i]) / (x[i + 1] - x[i])
+                    interpolated_value = a + b * (x_interpolate - x[i]) + c * (x_interpolate - x[i]) ** 2 + d * (x_interpolate - x[i]) ** 3
+                    return interpolated_value
+
+            print("Interpolation value is outside the range of provided data")
+
+        
+        coefficients = cubic_spline_interpolation(x, y)
+    
+        # Interpolate values within the current segment
+        min_x = np.min(x)
+        max_x = np.max(x)
+        
+        num_interpolation_points = 100  # Increase the number of points for smoother plots
+        interpolated_xs = np.linspace(min_x, max_x, num_interpolation_points)
+        
+        interpolated_values = []
+        for x_interpolate in interpolated_xs:
+            interpolated_value = cubic_spline_interpolate(x, coefficients, x_interpolate)
+            interpolated_values.append(interpolated_value)
+        
+        # Plot the original segment and the interpolated values
+        plt.plot(x, y, 'o', label='Original Segment')
+        plt.plot(interpolated_xs, interpolated_values, label='Interpolated Values')
+        
+
+
+class Interpolator:
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def set_strategy(self, strategy):
+        self.strategy = strategy
+
+    def interpolate(self, x, y):
+        return self.strategy.interpolate(x, y)
+
--- Scripts/main.py
+++ Scripts/main.py
@@ -44,119 +44,25 @@ for idx, segment in enumerate(segmented_arrays, start=1):
     for j, point in enumerate(segment):
         plt.text(point[0], point[1], str(j + 1), ha='center', va='center', fontsize=8)
         
+from interpolations import Interpolator, LinearPieceWiseInterpolation, CubicSplineInterpolation, LeastSquaresInterpolation
+
+# Example usage
+linear_strategy = LinearPieceWiseInterpolation()
+least_squares_strategy = LeastSquaresInterpolation()
+cubic_spline_strategy = CubicSplineInterpolation()
+
+# Initialize the Interpolator with a default strategy
+interpolator = Interpolator(cubic_spline_strategy)
 
 
-def piecewise_linear_approximation(x, y):
-    for i in range(len(x) - 1):
-        x_start, x_end = x[i], x[i + 1]
-        y_start, y_end = y[i], y[i + 1]
-
-        slope = (y_end - y_start) / (x_end - x_start)
-        intercept = y_start - slope * x_start
-
-        x_interpolated = np.linspace(x_start, x_end, 100)
-        y_interpolated = slope * x_interpolated + intercept
-
-        plt.plot(x_interpolated, y_interpolated, color='red')
-    
-    plt.scatter(x, y)
-
-
-def least_squares_approximation(x, y, degree=3):
-    A = np.vander(x, degree + 1, increasing=True)
-
-    # Check matrix rank
-    if np.linalg.matrix_rank(A) == A.shape[1]:  # Check if matrix is full rank
-        # Solve the normal equations to obtain coefficients
-        ATA = np.dot(A.T, A)
-        ATy = np.dot(A.T, y)
-        coeffs = np.linalg.solve(ATA, ATy)
-
-        # Generate points for smooth curve display
-        x_smooth = np.linspace(np.min(x), np.max(x), 100)
-        A_smooth = np.vander(x_smooth, degree + 1, increasing=True)
-        y_smooth = np.dot(A_smooth, coeffs)
-
-        # Plot each segment's points and the fitted polynomial curve
-        plt.scatter(x, y)
-        plt.plot(x_smooth, y_smooth, color='red')
-    else:
-        print("Matrix is not full rank, skipping segment fitting.")
-
-def cubic_spline_interpolation(x, y):
-    n = len(x)
-    if n != len(y):
-        print("Interpolation value is outside the range of provided data")
-
-
-    h = np.diff(x)
-    delta = np.diff(y) / h
-
-    # Construct the tridiagonal matrix
-    matrix = np.zeros((n, n))
-    matrix[0, 0] = 1
-    matrix[n - 1, n - 1] = 1
-
-    for i in range(1, n - 1):
-        matrix[i, i - 1] = h[i - 1]
-        matrix[i, i] = 2 * (h[i - 1] + h[i])
-        matrix[i, i + 1] = h[i]
-
-    rhs = np.zeros(n)
-    rhs[1:-1] = 3 * (delta[1:] - delta[:-1])
-
-    # Solve the linear system to find the coefficients
-    c = np.linalg.solve(matrix, rhs)
-
-    # Calculate the remaining coefficients
-    b = np.zeros(n - 1)
-    d = np.zeros(n - 1)
-    for i in range(n - 1):
-        b[i] = delta[i] - h[i] * (2 * c[i] + c[i + 1]) / 3
-        d[i] = (c[i + 1] - c[i]) / (3 * h[i])
-
-    coefficients = []
-    for i in range(n - 1):
-        coefficients.append((y[i], b[i], c[i], d[i]))
-
-    return coefficients
-
-def cubic_spline_interpolate(x, coefficients, x_interpolate):
-    for i in range(len(coefficients)):
-        if x_interpolate >= x[i] and x_interpolate <= x[i + 1]:
-            a, b, c, d = coefficients[i]
-            t = (x_interpolate - x[i]) / (x[i + 1] - x[i])
-            interpolated_value = a + b * (x_interpolate - x[i]) + c * (x_interpolate - x[i]) ** 2 + d * (x_interpolate - x[i]) ** 3
-            return interpolated_value
-
-    print("Interpolation value is outside the range of provided data")
 
 for segment in segmented_arrays:
     segment = segment[np.argsort(segment[:, 0])]
     x = segment[:, 0]
     y = segment[:, 1]
-   
 
-    coefficients = cubic_spline_interpolation(x, y)
-    
-    # Interpolate values within the current segment
-    min_x = np.min(x)
-    max_x = np.max(x)
-    
-    num_interpolation_points = 100  # Increase the number of points for smoother plots
-    interpolated_xs = np.linspace(min_x, max_x, num_interpolation_points)
-    
-    interpolated_values = []
-    for x_interpolate in interpolated_xs:
-        interpolated_value = cubic_spline_interpolate(x, coefficients, x_interpolate)
-        interpolated_values.append(interpolated_value)
-    
-    # Plot the original segment and the interpolated values
-    plt.plot(x, y, 'o', label='Original Segment')
-    plt.plot(interpolated_xs, interpolated_values, label='Interpolated Values')
 
-    # piecewise_linear_approximation(x,y)
-
+    interpolator.interpolate(x, y) # returns values too
 
 
 plt.xlabel('X-axis')
@@ -164,4 +70,3 @@ plt.ylabel('Y-axis')
 plt.title('Segments based on X-coordinate')
 plt.show()
 
-# ... (additional parts of the script)
