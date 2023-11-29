c09295048fb14d5daac580b27e6390a194ae95d1
DemetreShonia <shonka.demeloper@gmail.com>
2023-11-29 23:50:07 +0400

    Add Spline Implementation

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,7 +6,7 @@ from edge_detection import EdgeDetection
 from clustering import kmeans, find_closest_point, create_closed_loop, segment_points
 
 # Load the image and perform edge detection
-img_processor = ImageProcessor('curve2.jpg')
+img_processor = ImageProcessor('curve3.jpg')
 pixels = img_processor.get_image_matrix()
 width, height = img_processor.get_image_size()
 
@@ -62,18 +62,7 @@ def piecewise_linear_approximation(x, y):
     plt.scatter(x, y)
 
 
-for segment in segmented_arrays:
-    segment = segment[np.argsort(segment[:, 0])]
-    x = segment[:, 0]
-    y = segment[:, 1]
-
-    # Perform linear piecewise approximation for each segment
-
-    # piecewise_linear_approximation(x, y)
-
-    # Fit a polynomial curve to each segment using the least squares method
-    # Create the Vandermonde matrix
-    degree = 3
+def least_squares_approximation(x, y, degree=3):
     A = np.vander(x, degree + 1, increasing=True)
 
     # Check matrix rank
@@ -94,7 +83,80 @@ for segment in segmented_arrays:
     else:
         print("Matrix is not full rank, skipping segment fitting.")
 
-    # THIS PART IS WHERE ALGORITHM WILL CHANGE (STRATEGY)
+def cubic_spline_interpolation(x, y):
+    n = len(x)
+    if n != len(y):
+        print("Interpolation value is outside the range of provided data")
+
+
+    h = np.diff(x)
+    delta = np.diff(y) / h
+
+    # Construct the tridiagonal matrix
+    matrix = np.zeros((n, n))
+    matrix[0, 0] = 1
+    matrix[n - 1, n - 1] = 1
+
+    for i in range(1, n - 1):
+        matrix[i, i - 1] = h[i - 1]
+        matrix[i, i] = 2 * (h[i - 1] + h[i])
+        matrix[i, i + 1] = h[i]
+
+    rhs = np.zeros(n)
+    rhs[1:-1] = 3 * (delta[1:] - delta[:-1])
+
+    # Solve the linear system to find the coefficients
+    c = np.linalg.solve(matrix, rhs)
+
+    # Calculate the remaining coefficients
+    b = np.zeros(n - 1)
+    d = np.zeros(n - 1)
+    for i in range(n - 1):
+        b[i] = delta[i] - h[i] * (2 * c[i] + c[i + 1]) / 3
+        d[i] = (c[i + 1] - c[i]) / (3 * h[i])
+
+    coefficients = []
+    for i in range(n - 1):
+        coefficients.append((y[i], b[i], c[i], d[i]))
+
+    return coefficients
+
+def cubic_spline_interpolate(x, coefficients, x_interpolate):
+    for i in range(len(coefficients)):
+        if x_interpolate >= x[i] and x_interpolate <= x[i + 1]:
+            a, b, c, d = coefficients[i]
+            t = (x_interpolate - x[i]) / (x[i + 1] - x[i])
+            interpolated_value = a + b * (x_interpolate - x[i]) + c * (x_interpolate - x[i]) ** 2 + d * (x_interpolate - x[i]) ** 3
+            return interpolated_value
+
+    print("Interpolation value is outside the range of provided data")
+
+for segment in segmented_arrays:
+    segment = segment[np.argsort(segment[:, 0])]
+    x = segment[:, 0]
+    y = segment[:, 1]
+   
+
+    coefficients = cubic_spline_interpolation(x, y)
+    
+    # Interpolate values within the current segment
+    min_x = np.min(x)
+    max_x = np.max(x)
+    
+    num_interpolation_points = 100  # Increase the number of points for smoother plots
+    interpolated_xs = np.linspace(min_x, max_x, num_interpolation_points)
+    
+    interpolated_values = []
+    for x_interpolate in interpolated_xs:
+        interpolated_value = cubic_spline_interpolate(x, coefficients, x_interpolate)
+        interpolated_values.append(interpolated_value)
+    
+    # Plot the original segment and the interpolated values
+    plt.plot(x, y, 'o', label='Original Segment')
+    plt.plot(interpolated_xs, interpolated_values, label='Interpolated Values')
+
+    # piecewise_linear_approximation(x,y)
+
 
 
 plt.xlabel('X-axis')
